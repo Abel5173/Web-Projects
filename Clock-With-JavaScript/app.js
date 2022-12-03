8a706bf7ea2317fde5b21b89098000eeea1fb3dd
Abel <abelzeleke5173@gmail.com>
2022-12-03 19:22:32 +0300

    Just checking

---
+++ Clock-With-JavaScript/app.js
@@ -0,0 +1,9 @@
+setInterval(setClock, 1000)
+
+function setClock(){
+    const currentDate = new Date()
+    const secondRatio = currentDate.getSeconds()/60
+    const minutesRatio = currentDate.getSeconds()/60
+    const secondRatio = currentDate.getSeconds()/60
+    const secondRatio = currentDate.getSeconds()/60
+}
