7201dfc31fc080d3a3b13d897232c9ab32bf01d8
Abel <abelzeleke5173@gmail.com>
2022-12-12 00:11:49 +0300

    Finished at last

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -1,7 +1,9 @@
 document.querySelector('#push').onclick = function(){
+    // Validate input field
     if (document.querySelector('#newtask input').value.length == 0){
         alert("Please Enter a Task!")
     }
+    // Add a new Task
     else{
         document.querySelector('#tasks').innerHTML += `
         <div class="task">
@@ -12,19 +14,22 @@ document.querySelector('#push').onclick = function(){
             <i class="fa fa-trash-o" aria-hidden="true"></i>
             </button>
         </div>
-        `
+      `
+// Deleting a Task
         var current_tasks = document.querySelectorAll('.delete');
         for(var i=0; i<current_tasks.length; i++){
             current_tasks[i].onclick = function(){
                 this.parentNode.remove();
             }
         }
-
-        var tasks = document.querySelectorAll(".tasks")
+// Crossing out a completed task
+        var tasks = document.querySelectorAll(".task")
         for(var i=0; i<tasks.length; i++){
             tasks[i].onclick = function(){
                 this.classList.toggle('completed')
             }
         }
+        // Clearing input field after each entry
+        document.querySelector("#newtask input").value=""
     }
 }
